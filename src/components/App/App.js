a08162c6fdf6d009571c48f2bf0d8c1ae7b9adcf
ejay <ejay.aguirre04@gmail.com>
2022-06-25 03:45:52 +1000

    inital commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand text-primary" href="#">
+            <img src="https://lemon-music.sfo3.digitaloceanspaces.com/Screen%20Shot%202022-06-25%20at%203.40.56%20AM.png" alt="logo" width="30" height="24" className="d-inline-block align-text-top" />
+            Bootstrap
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+const clientID = '897a402bd6574d98b59a6bd88763cffe';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // check accessToken
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // clears the tokens
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            window.location = accessURL
+        }
+    }
+}
+
+export default Spotify
